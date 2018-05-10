9e7d7e6654842dda50a37265304a0baa4afab1cf
Nick Chapman <nickpchapman@gmail.com>
2018-05-09 17:43:14 -0700

    add bulma form and dependency

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Form.css";
 
 class Form extends Component {
   constructor(props) {
@@ -7,7 +8,55 @@ class Form extends Component {
   }
 
   render() {
-    return <div>Form Component</div>;
+    return (
+      <div class="box main-form">
+        <div class="columns">
+          <div class="column">
+            <div class="field">
+              <label class="label">Leaving From</label>
+              <div class="control">
+                <div class="select">
+                  <select>
+                    <option>Select dropdown</option>
+                    <option>With options</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="field">
+              <label class="label">Arriving At</label>
+              <div class="control">
+                <div class="select">
+                  <select>
+                    <option>Select dropdown</option>
+                    <option>With options</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <label class="radio">
+              <input type="radio" name="question" />
+              Arrive By
+            </label>
+            <label class="radio">
+              <input type="radio" name="question" />
+              Leave By
+            </label>
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link">Submit</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
