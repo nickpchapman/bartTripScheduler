521b1beee1194c1fd06c43c5994b0a082dca07a2
Nick Chapman <nickpchapman@gmail.com>
2018-05-11 17:06:03 -0700

    replace variable declarations made with 'let' in server to 'const'

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/stnList", (req, res) => {
       "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y"
     )
     .then(response => {
-      let stnInfo = response.data.root.stations;
+      const stnInfo = response.data.root.stations;
 
       //add station abbr hash for station name lookup
       stnInfo.lookup = stnInfo.station.reduce((hash, stn) => {
@@ -35,16 +35,16 @@ app.get("/newSchedule", (req, res) => {
   const head = "http://api.bart.gov/api/sched.aspx?";
   const tail = "&b=2&a=2&json=y";
   const key = "&key=MW9S-E7SL-26DU-VV8V";
-  let type = "cmd=" + req.query.type;
-  let orig = "&orig=" + req.query.orig;
-  let dest = "&dest=" + req.query.dest;
-  let date = "&date=" + req.query.date;
-  let time = "&time=" + req.query.time;
+  const type = "cmd=" + req.query.type;
+  const orig = "&orig=" + req.query.orig;
+  const dest = "&dest=" + req.query.dest;
+  const date = "&date=" + req.query.date;
+  const time = "&time=" + req.query.time;
 
   axios
     .get(head + type + key + orig + dest + date + time + tail)
     .then(response => {
-      let scheduleInfo = response.data.root;
+      const scheduleInfo = response.data.root;
 
       //add 'arrive' or 'depart' value to indicate request type
       scheduleInfo.tripType = req.query.type;
